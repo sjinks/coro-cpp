06119cc6c469a0679c0b702cd56b6c95bd5fae19
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-02-07 00:07:40 +0200

    fix: cmake configuration

--- cmake/wwa-coro-config.cmake
+++ cmake/wwa-coro-config.cmake
@@ -2,6 +2,7 @@ get_filename_component(CORO_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 list(APPEND CMAKE_MODULE_PATH ${CORO_CMAKE_DIR})
 
-if(NOT TARGET wwa::coro)
-    include("${JSONRPC_CMAKE_DIR}/wwa-coro-target.cmake")
+if(NOT TARGET wwa-coro)
+    include("${CORO_CMAKE_DIR}/wwa-coro-target.cmake")
+    add_library(wwa::coro ALIAS wwa-coro)
 endif()
